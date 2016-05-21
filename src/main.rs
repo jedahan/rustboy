192739601d487f849a592b2d27ed6866f9cb42ad
Jonathan Dahan <jonathan@jonathan.is>
2016-05-21 12:21:23 -0400

    add some more headers

--- src/main.rs
+++ src/main.rs
@@ -15,26 +15,84 @@ fn main() {
         length: usize
     };
 
+    impl Default for Range {
+        fn default () -> Range {
+            Range { offset: 0, length: 1 }
+        }
+    };
+
     struct Header {
         name: &'static str,
+        format: &'static str,
         range: Range
     };
 
-    let name_header = Header {
+    impl Default for Header {
+        fn default () -> Header {
+            Header {
+                name: "default",
+                format: "",
+                range: Range { ..Default::default() }
+            }
+        }
+    };
+
+    let name = Header {
         name: "title",
+        format: "string",
         range: Range {
             offset: 0x134,
             length: 0x10
         }
     };
 
-    let headers = vec![name_header];
+    let sgb = Header {
+        name: "super game boy flag",
+        range: Range {
+            offset: 0x146,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let cart_type = Header {
+        name: "cart type (mappers)",
+        range: Range {
+            offset: 0x147,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let rom_size = Header {
+        name: "rom size",
+        range: Range {
+            offset: 0x148,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let non_japanese = Header {
+        name: "non_japanese",
+        range: Range {
+            offset: 0x14A,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    let headers = vec![name, sgb, cart_type, rom_size, non_japanese];
 
     for header in headers {
         let start = header.range.offset;
         let end = header.range.offset + header.range.length;
         let mut header_slice = &buffer[start..end];
-        println!("{}: {}", header.name, String::from_utf8_lossy(&mut header_slice));
+        if header.format == "string" {
+            println!("{}: {}", header.name, String::from_utf8_lossy(&mut header_slice));
+        } else {
+            println!("{}: {:?}", header.name, header_slice);
+        }
     }
 
 }
