b18821d46bdb8ac66771445eb9ba6b5c4a4e3f43
Jonathan Dahan <jonathan@jonathan.is>
2016-05-29 14:26:25 -0400

    Initialize the registers to correct values on reset
    These values were taken from page 18 of GB.pdf
    
    I took out the wram/vram from the Cpu for two reasons
    
    The first reason is I was having trouble initializing them to random
    values. Second reason, is that the memory should be abstracted away
    and I can use the Index<InclusiveRange> Trait to make access
    transparent, so yay!
    
    I will still have to figure out how to make wram/vram all random, but
    thats for another day. I want to start implementing opcodes soon!

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,61 @@ use lib::cart;
 struct Cpu {
     pc: u16,
     sp: u16,
-    registers: [u8; 8],
-    wram: [u8; 1024],
-    vram: [u8; 1024]
+    reg_a: u8,
+    reg_f: u8,
+
+    reg_b: u8,
+    reg_c: u8,
+
+    reg_d: u8,
+    reg_e: u8,
+
+    reg_h: u8,
+    reg_l: u8
 }
 
 impl Cpu {
     fn new() -> Cpu {
         Cpu {
-            pc: 0x0100,
-            sp: 0xFFFE,
-            // A, B, D, H, F, C, E, L?
-            registers: [0 as u8; 8],
-            wram: [0; 1024],
-            vram: [0; 1024]
+            pc: 0,
+            sp: 0,
+            reg_a: 0,
+            reg_f: 0,
+
+            reg_b: 0,
+            reg_c: 0,
+
+            reg_d: 0,
+            reg_e: 0,
+
+            reg_h: 0,
+            reg_l: 0
         }
+    }
+
+    fn reset(&mut self) {
+        self.pc = 0x0100;
+        self.sp = 0xFFFE;
+        self.reg_a = 0x01;
+        self.reg_f = 0xB0;
+
+        self.reg_b = 0x00;
+        self.reg_c = 0x13;
+
+        self.reg_d = 0x00;
+        self.reg_e = 0xD8;
+
+        self.reg_h = 0x01;
+        self.reg_l = 0x4D;
     }
 }
 
 impl fmt::Display for Cpu {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "{:?}", self.registers);
+        writeln!(f, "af: {:0>2X}{:0>2X}", self.reg_a, self.reg_f);
+        writeln!(f, "bc: {:0>2X}{:0>2X}", self.reg_b, self.reg_c);
+        writeln!(f, "de: {:0>2X}{:0>2X}", self.reg_d, self.reg_e);
+        writeln!(f, "hl: {:0>2X}{:0>2X}", self.reg_h, self.reg_l);
         writeln!(f, "pc: {:0>4X}", self.pc);
         writeln!(f, "sp: {:0>4X}", self.sp);
         Ok(())
