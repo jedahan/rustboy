a1908e833d812a261057675f96c92f262b3ce695
Jonathan Dahan <jonathan@jonathan.is>
2016-06-02 22:49:54 -0400

    abuse macros to have header! => header::Header::new

--- src/header.rs
+++ src/header.rs
@@ -8,11 +8,11 @@ pub struct Header {
 }
 
 impl Header {
-    pub fn new(name: &'static str, range: Range<usize>) -> Header {
+    pub fn new(name: &'static str, range: Range<usize>, format: &'static str) -> Header {
         Header {
             name: name,
             range: range,
-            format: ""
+            format: format
         }
     }
 }
