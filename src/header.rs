a64e139497bb74007057387d8fe971e0900a58ca
Jonathan Dahan <jonathan@jonathan.is>
2016-06-04 12:38:33 -0400

    cleanup

--- src/header.rs
+++ src/header.rs
@@ -3,8 +3,8 @@ use std::ops::Range;
 #[derive(Debug)]
 pub struct Header {
     pub name: &'static str,
+    pub range: Range<usize>,
     pub format: &'static str,
-    pub range: Range<usize>
 }
 
 impl Header {
