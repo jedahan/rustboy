336c149a1b2105eff0e4cfb32c1f0ce399209000
Jonathan Dahan <hi@jonathan.is>
2016-07-30 12:54:34 -0400

    Fix tests and bump dependencies

--- src/main.rs
+++ src/main.rs
@@ -51,14 +51,18 @@ fn load_cart<P: AsRef<Path>>(path: P) -> cart::Cart {
 
 #[test]
 fn checksums() {
+    println!("");
     let dir = Path::new("roms");
     if fs::metadata(dir).unwrap().is_dir() {
         for entry in fs::read_dir(dir).unwrap() {
             let entry = entry.unwrap();
             if entry.file_type().unwrap().is_file() {
                 let filepath = entry.path();
-                println!("testing {:?}", filepath);
-                assert!(load_cart(filepath).is_valid());
+                // We only test against official cartridges, not homebrew
+                if filepath.file_name().unwrap().to_string_lossy().contains("(") {
+                    println!("testing {:?}", filepath);
+                    assert!(load_cart(filepath).is_valid());
+                }
             }
         }
     }
