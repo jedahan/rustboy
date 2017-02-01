04ae3b90ee3ae77ec70b605b51a836799b089f45
Jonathan Dahan <jdehan@etsy.com>
2017-02-01 14:17:57 -0500

    Add crc32 check to dmg_rom.bin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate crc;
+
 mod gameboy;
 mod cpu;
 mod memory;
@@ -11,6 +13,7 @@ use std::env;
 use std::fs;
 use std::io::Read;
 use std::path::Path;
+use crc::crc32;
 
 fn main() {
     let boot = load_bootrom(Path::new("dmg_rom.bin"));
@@ -23,21 +26,28 @@ fn main() {
     gameboy::run(boot, cart);
 }
 
-fn load_bootrom<P: AsRef<Path>>(path: P) -> [u8; gameboy::BOOTROM_SIZE] {
-    let mut file = fs::File::open(path).unwrap();
+fn load_bootrom(path: &Path) -> [u8; gameboy::BOOTROM_SIZE] {
+    let mut file = fs::File::open(path)
+        .expect("Please download dmg_rom.bin");
+
     let mut buffer = [0; gameboy::BOOTROM_SIZE];
     file.read_exact(&mut buffer).unwrap();
+
+    let dmg_rom_crc32 = 0x59c8598e;
+    assert_eq!(crc32::checksum_ieee(&buffer), dmg_rom_crc32,
+        "{:?} has invalid crc32, expected {:x}", path, dmg_rom_crc32);
+
     buffer
 }
 
-fn load_rom<P: AsRef<Path>>(path: P) -> Vec<u8> {
+fn load_rom(path: &Path) -> Vec<u8> {
     let mut file = fs::File::open(path).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     buffer
 }
 
-fn load_cart<P: AsRef<Path>>(path: P) -> cart::Cart {
+fn load_cart(path: &Path) -> cart::Cart {
     cart::Cart::new(load_rom(path))
 }
 
